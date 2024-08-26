1c3151596867fd740d9e2c33fc357ea20267bf55
Said Nurullin <s.nurullin@innopolis.university>
2024-08-26 15:43:25 +0300

    Lab 1 finished

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -57,33 +57,66 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text("Hello Flutter!",
+    style: TextStyle(fontSize: 14, color: Colors.red, fontWeight: FontWeight.bold),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
+    return const Icon(
+      Icons.home,
       size: 100,
+      color: Colors.blue,
     );
   }
 
   Widget exercise3() {
     // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
-    );
+    return Image.network(
+    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+    width: 200,
+    height: 200,
+    fit: BoxFit.cover,
+  );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return TextButton(onPressed: () { 
+      print("Pressed");
+    },
+    child: const Text('Free money!'),
+  );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+        padding: const EdgeInsets.all(20.0), // Padding inside the container
+        margin: const EdgeInsets.only(left: 10.0), // Margin below the container
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.green, width: 2.0), // Border around the container
+          color: const Color.fromARGB(255, 171, 221, 114), // Background color
+        ),
+        child: const Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 30),
+        ),
+        ),
+      Container(
+        padding: const EdgeInsets.all(16.0),
+        margin: const EdgeInsets.only(top: 8.0), // Margin above the container
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), // Rounded corners
+          color: Colors.blue.shade200, // Background color
+        ),
+        child: const Icon(
+          Icons.favorite,
+          size: 42,
+          color: Colors.red,
+        ),
+      ),
+      ],
     );
   }
 }
