203b8159a012699d0cabb3403618fb4a585952e4
paranid5 <paranid5@paranid5s-MacBook-Pro.local>
2024-08-26 15:59:02 +0300

    lab 1 done

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -6,84 +6,115 @@ class Lab1 extends StatelessWidget {
   const Lab1({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LAb1HomePage(),
-    );
-  }
+  Widget build(BuildContext context) =>
+      const MaterialApp(home: LAb1HomePage());
 }
 
 class LAb1HomePage extends StatelessWidget {
   const LAb1HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Lab 1'),
-      ),
-      body: myWidget(),
-    );
-  }
+  Widget build(BuildContext context) =>
+      Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Lab 1'),
+        ),
+        body: myWidget(),
+      );
 
-  Widget myWidget() {
-    return SingleChildScrollView(
-      child: Column(
+  Widget myWidget() =>
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            exercise1(),
+            const SizedBox(height: 25),
+            exercise2(),
+            const SizedBox(height: 25),
+            exercise3(),
+            const SizedBox(height: 25),
+            exercise4(),
+            const SizedBox(height: 25),
+            exercise5(),
+            const SizedBox(height: 25),
+          ],
+        ),
+      );
+
+  Widget exercise1() =>
+      const Text(
+        'Hello, Flutter!',
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFFCA33FF),
+        ),
+      );
+
+  Widget exercise2() =>
+      const Icon(
+        Icons.icecream,
+        size: 128,
+        color: Color(0xFFFFE333),
+      );
+
+  Widget exercise3() =>
+      const Image(
+        image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVLl8BiY-frBPZV4m1fTBaHrex836G4LyVKA&s'),
+        width: 128,
+        height: 256,
+        fit: BoxFit.fill,
+      );
+
+  Widget exercise4() =>
+      TextButton(
+        onPressed: () => print('Pressed'),
+        style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.blueAccent),
+            shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                ),
+            ),
+        ),
+        child: const Text(
+            'Press',
+            style: TextStyle(color: Colors.white),
+        ),
+      );
+
+  Widget exercise5() =>
+      Column(
         children: [
-          const SizedBox(
-            height: 25,
+          Container(
+              padding: const EdgeInsets.only(top: 8, bottom: 8, right: 8, left: 8),
+              margin: const EdgeInsets.only(top: 16, bottom: 16, right: 16, left: 16),
+              decoration: const BoxDecoration(
+                color: Colors.pinkAccent,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                    bottomLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
+                )
+              ),
+              child: const Text("Some text"),
           ),
-          exercise1(),
-          const SizedBox(
-            height: 25,
+          Container(
+            padding: const EdgeInsets.only(top: 8, bottom: 8, right: 8, left: 8),
+            margin: const EdgeInsets.only(top: 16, bottom: 16, right: 16, left: 16),
+            decoration: const BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                )
+            ),
+            child: const Icon(Icons.adb, color: Colors.greenAccent),
           ),
-          exercise2(),
-          const SizedBox(
-            height: 25,
-          ),
-          exercise3(),
-          const SizedBox(
-            height: 25,
-          ),
-          exercise4(),
-          const SizedBox(
-            height: 25,
-          ),
-          exercise5(),
         ],
-      ),
-    );
-  }
-
-  Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
-
-  Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
-
-  Widget exercise3() {
-    // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
-
-  Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
-
-  Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
+      );
 }
