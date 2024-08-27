c791657c5a8816e162f82fdd3cb78e3c4515be1b
DeHopen <a2302004b23@yandex.ru>
2024-08-27 14:33:25 +0300

    Lab 1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -21,6 +21,7 @@ class LAb1HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        leading: const Icon(Icons.stars),
         title: const Text('Lab 1'),
       ),
       body: myWidget(),
@@ -57,33 +58,74 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      "Hello Flutter!",
+      style: TextStyle(fontWeight: FontWeight.bold,
+          fontSize: 25, color: Colors.red),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.abc_sharp,
+      size: 52,
+      color: Colors.black,
     );
   }
 
   Widget exercise3() {
-    // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
+    return Image.network(
+      'https://pichold.ru/wp-content/uploads/2019/06/05-09-8158890_1.jpg',
+      width: 300,
+      height: 200,
+      fit: BoxFit.cover,
     );
+
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextButton(
+          onPressed: () {
+            print("Pressed");
+          },
+          child: const Text('Tap me to see "Pressed"'),
+
+        ),
+      ],
     );
+
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          margin: const EdgeInsets.fromLTRB(10, 5, 10, 5) ,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.deepOrange),
+            color: const Color.fromARGB(255, 216, 160, 100),
+          ),
+          child: const Text(
+            'White Cat',
+            style: TextStyle(fontSize: 27),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.green),
+            color: Colors.white,
+          ),
+          child: const Icon(Icons.access_alarm, color: Colors.grey),
+        ),
+      ],
+
+
     );
   }
 }
