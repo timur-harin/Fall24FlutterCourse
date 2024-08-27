600e8510fc2c52823149286cd7f207a905d8ea7a
Elina <elina_metalik55@mail.ru>
2024-08-27 14:44:59 +0300

    Elina Murtazina @el.murtazina.innopolis.university

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Lab1());
@@ -57,33 +59,73 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+
+    return const Text(
+      "Hello Flutter!",
+      style: TextStyle(fontSize: 28, color: Color.fromARGB(200, 42, 3, 75), fontWeight: FontWeight.w300)
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.home,
+      size: 100,  // Setting the size of the icon
+      color: Color.fromARGB(255, 178, 121, 72), // Correct color value with opacity
     );
   }
 
   Widget exercise3() {
     // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
+    return const Image(
+      image: NetworkImage("https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"),
+      width: 98,
+      height: 180,
+      fit: BoxFit.fill //intentional
     );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+  return TextButton(
+    onPressed: () {
+      print('pressed');
+    },
+    style: ButtonStyle(
+      backgroundColor: WidgetStateProperty.all(const Color.fromARGB(255, 5, 79, 168)),
+    ),
+    child: const Text("button",
+    style: TextStyle(fontSize: 28, color: Color.fromARGB(255, 240, 236, 243), fontWeight: FontWeight.w300)),
     );
   }
 
+
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(10.0), // External margin
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 5.0, bottom: 5.0), // Internal padding
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(124, 160, 200, 0), // Background color
+            borderRadius: BorderRadius.circular(10.0), // Rounded corners
+            border: Border.all(
+              color: Color.fromARGB(255, 29, 82, 9), // Border color
+              width: 4.0, // Border width
+            ),
+          ),
+          child: const Text("Look \nhere",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 28, color: Color.fromARGB(255, 138, 99, 0), fontWeight: FontWeight.w100)
+        )
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 95.0), // External margin
+          child: const Icon(
+            Icons.arrow_circle_up_sharp,
+            size: 100,  // Setting the size of the icon
+            color: Color.fromARGB(255, 29, 82, 9), // Correct color value with opacity
+          ),
+        )
+      ],
     );
   }
 }
