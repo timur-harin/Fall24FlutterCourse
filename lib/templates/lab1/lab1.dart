19214f5bfd06c08ab9872413d2c83dc07e403eb5
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-08-27 11:56:32 +0300

    Lab 1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -57,33 +57,75 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      'Hello Flutter!',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Color.fromARGB(255, 124, 4, 134),
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.youtube_searched_for,
+      size: 50.0,
+      color: Color.fromARGB(255, 124, 4, 134),
     );
   }
 
   Widget exercise3() {
-    // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
+    return Image.network(
+      'https://i.imgur.com/AXSTny2.jpeg',
+      width: 300.0,
+      height: 200.0,
+      fit: BoxFit.cover,
     );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      onPressed: () {
+        print('Pressed');
+      },
+      child: Text('Feed Him'),
     );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 251, 187, 251),
+          border: Border.all(color: const Color.fromARGB(255, 124, 4, 134), width: 2.0),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: const Text(
+          'His name is McNuggets',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.green[100],
+          border: Border.all(color: Colors.green, width: 2.0),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: const Icon(
+          Icons.thumb_up,
+          size: 40.0,
+          color: Colors.green,
+        ),
+      ),
+    ],
+  );
 }
+}
+
