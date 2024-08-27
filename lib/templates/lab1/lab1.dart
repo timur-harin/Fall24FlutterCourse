fd504f5f23a1eb4005e4a5d87d67f29d9bafe778
anyar <anyarylova51@gmail.com>
2024-08-27 15:29:09 +0300

    Main Anna Rylova a.rylova@innopolis.university

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Lab1());
@@ -57,33 +61,38 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return const Text("Hello Flutter!", style: TextStyle(fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold));
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return const Icon(Icons.catching_pokemon, size: 100, color: Colors.red);
   }
 
   Widget exercise3() {
     // Example for image from internet 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'
-    return const FlutterLogo(
-      size: 100,
-    );
+    return Image.network('https://www.meme-arsenal.com/memes/958634784ef9bb39322a28810652347d.jpg', width: 250, height: 200,fit: BoxFit.cover);
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return TextButton(onPressed: () {
+      print("Pressed");
+    }, child: const Text('Press this button'));
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        margin: EdgeInsets.all(10), 
+        padding: EdgeInsets.all(10), 
+        decoration: BoxDecoration(color: Colors.lightBlue, 
+        border: Border.all(color: Colors.deepPurple, width: 1)), 
+        child: Text('Hello Flutter!', style: TextStyle(color: Colors.white, fontSize: 20))),
+      Container(
+        margin: EdgeInsets.all(10), 
+        padding: EdgeInsets.all(10), 
+        decoration: BoxDecoration(color: Colors.lightBlue, 
+        border: Border.all(color: Colors.deepPurple, width: 1)), 
+        child: Icon(Icons.monitor, size: 80, color: Colors.white)),
+    ]);
   }
 }
