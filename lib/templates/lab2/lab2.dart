bcb53c48365340aff97824ea4f2240890d03b58f
Kuklin Pavel <kuklinpavel2004@gmail.com>
2024-08-29 20:00:30 +0300

    lab2 completed

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,15 +1,72 @@
+void main() {
+  Book deadSoulsBook = Book('Мертвые души', 'Н. В. Гоголь', 305);
+  deadSoulsBook.bookDetails();
+
+  Book otherBook = FictionBook('Otherside', 'Unknown', 112, );
+  otherBook.bookDetails();
+
+  LibraryBook libraryDeadSoulsBook = LibraryBook('Мертвые души', 'Н. В. Гоголь', 305);
+  libraryDeadSoulsBook.read();
+
+  print("Calculated area for rect 2x4: ${calculateArea(2, 4)}");
+  print("Calculated area for circle r=3: ${calculateArea(3, 0, shape: 'circle')}");
+
+  User admin = User('Paul', age: 34);
+  admin.printUserDetails();
+}
+
 class Book {
-  // your code here
+  String title;
+  String author;
+  int numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('Book: "$title" by $author; $numberOfPages pages.');
+  }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book{
+  String genre = 'fiction';
+
+  FictionBook(super.title, super.author, super.numberOfPages);
+
+  @override
+  void bookDetails() {
+    print('Book: "$title" by $author of $genre genre; $numberOfPages pages.');
+  }
 }
 
-mixin JsonSerializable {
-  // your code here
+mixin Readable {
+  void read() {
+    print("The book is being read...");
+  }
+}
+
+class LibraryBook extends Book with Readable {
+  LibraryBook(super.title, super.author, super.numberOfPages);
+}
+
+double calculateArea(double width, double height, {String shape = 'rectangle'})
+{
+  if (shape == 'rectangle') {
+    return width * height;
+  } else if (shape == 'circle') {
+    return 3.14 * width * width;
+  } else {
+    throw ArgumentError('Argument shape can accept only "rectangle" [default] or "circle"');
+  }
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, {this.age, this.email});
+
+  void printUserDetails() {
+    print('Name: $name. Age: ${age ?? 'N/A'}. Email: ${email ?? 'N/A'}');
+  }
 }
