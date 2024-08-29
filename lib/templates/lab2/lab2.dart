7a8729097e756a51b5ad2017ed27d356d1d9ebda
mihdenis85 <mihdenis777@gmail.com>
2024-08-29 19:25:14 +0300

    Lab2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,15 +1,105 @@
-class Book {
-  // your code here
+void main() {
+  Book myBook = Book(
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    numberOfPages: 180,
+  );
+
+  myBook.bookDetails();
+  myBook.read();
+
+  FictionBook myFictionBook = FictionBook(
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    numberOfPages: 180,
+    genre: "Romance",
+  );
+
+  myFictionBook.bookDetails();
+  myBook.read();
+
+  print('');
+
+  double area = calculateArea(
+    length: 10.0,
+    width: 5.5
+  );
+  print(area);
+
+  double circleArea = calculateArea(
+    length: 4.3,
+    width: 4.3,
+    shape: "circle"
+  );
+  print(circleArea);
+
+  print('');
+
+  User user = User(
+    name: "John"
+  );
+  user.printUserDetails();
+
+  User user2 = User(
+    name: "Denis",
+    age: 20,
+    email: "d.mikhailov@innopolis.university"
+  );
+  user2.printUserDetails();
 }
 
-class FictionBook {
-  // your code here
+class Book with Readable {
+  String title;
+  String author;
+  int numberOfPages;
+
+  Book({required this.title, required this.author, required this.numberOfPages});
+
+  void bookDetails() {
+    print("Title: $title, Author: $author, Number of pages: $numberOfPages");
+  }
 }
 
-mixin JsonSerializable {
-  // your code here
+class FictionBook extends Book with Readable {
+  String genre;
+
+  FictionBook({
+    required String title,
+    required String author,
+    required int numberOfPages,
+    required this.genre,
+  }): super(title: title, author: author, numberOfPages: numberOfPages);
+
+  @override
+  void bookDetails() {
+    print("Title: $title, Author: $author, Number of pages: $numberOfPages, Genre: $genre");
+  }
+}
+
+mixin Readable {
+  void read() {
+    print("Reading...");
+  }
+}
+
+double calculateArea({required double length, required double width, String shape = "rectangle"}) {
+  if (shape == "circle") {
+    return 3.14 * length * length;
+  } else {
+    return length * width;
+  }
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+
+  User({required this.name, this.age, this.email});
+
+  void printUserDetails() {
+    print("Name: $name");
+    print("Age: ${age ?? 'N/A'}");
+    print("Email: ${email ?? 'N/A'}");
+  }
 }
