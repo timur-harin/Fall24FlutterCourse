e561e16fa365a73178f71649737b2e3062728dd8
iucdcs1 <d.vasilev@innopolis.university>
2024-08-29 19:11:05 +0300

    Completed Lab02

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,15 +1,82 @@
-class Book {
-  // your code here
+class Book with Readable {
+  String title;
+  String author;
+  int numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() =>
+      print('Title: $title, Author: $author, Number of pages: $numberOfPages');
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  String genre;
+
+  FictionBook(super.title, super.author, super.numberOfPages, this.genre);
+
+  @override
+  void bookDetails() => print(
+      'Title: $title, Author: $author, Genre: $genre, Number of pages: $numberOfPages');
 }
 
-mixin JsonSerializable {
-  // your code here
+mixin Readable {
+  void read() {
+    print("The book is being read");
+  }
 }
 
-class User{
-  // your code here
+class User {
+  String name;
+  int? age;
+  String? email;
+
+  User({required this.name, this.age, this.email});
+
+  void printUserDetails() {
+    print('Name: $name Age: ${age ?? 'N/A'} Email: ${email ?? 'N/A'}');
+  }
+}
+
+double calculateArea(
+    {required double length,
+    required double width,
+    String shape = 'rectangle'}) {
+  if (shape.toLowerCase() == 'rectangle') {
+    return length * width;
+  } else if (shape.toLowerCase() == 'circle') {
+    print('Length considered as the radius of the circle');
+    return 3.14 * length * length;
+  } else {
+    throw ArgumentError("'$shape' is an invalid shape.");
+  }
+}
+
+void main() {
+  // Classes
+  var book1 = Book('Some Book', 'Some Author', 4602);
+  book1.bookDetails();
+  // Mixin 
+  book1.read();
+
+  // Inheritance
+  var fictionBook1 = FictionBook('Justice', '???', 600, 'Detective');
+  fictionBook1.bookDetails();
+  // Mixin
+  fictionBook1.read();
+  
+
+  // Function
+  try {
+    print(calculateArea(length: 3, width: 3, shape: "circle"));
+    print(calculateArea(length: 3, width: 18));
+    print(calculateArea(length: 3, width: 7, shape: "test"));
+  } catch (e) {
+    print("Error: ${e.toString()}");
+  }
+
+  // Null-safety
+  User user1 = User(name: 'Test 1', age: 20, email: 'test.mail@innopolis.university');
+  user1.printUserDetails();
+  User user2 = User(name: 'Test 2');
+  user2.printUserDetails();
 }
