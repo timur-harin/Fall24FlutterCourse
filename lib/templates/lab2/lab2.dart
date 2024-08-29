3c2c7553e2ba66201c466b9f4aa694a38d35681f
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-08-29 22:21:59 +0600

    lab2:classes
    Create a Dart class named Book with corresponding properties and method named bookDetails that prints a string containing all the details

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,5 +1,14 @@
 class Book {
-  // your code here
+  String title;
+  String author;
+  int numberOfPages;
+
+  // Constructor that initializes all the properties
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('Title: $title, Author: $author, Number of pages: $numberOfPages');
+  }
 }
 
 class FictionBook {
@@ -12,4 +21,10 @@ mixin JsonSerializable {
 
 class User{
   // your code here
+}
+
+void main() {
+  // Creating an instance of Book
+  var book = Book('The Three-Body Problem', 'Liu Cixin', 400);
+  book.bookDetails();
 }
