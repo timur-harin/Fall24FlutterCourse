c5c468d150e7f65e26d16bffa1df78d66039d979
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-08-29 22:50:09 +0600

    lab2:function
    Create function named calculateArea that takes two named parameters length and width and an optional parameter shape of type String with a default value of "rectangle". It returns the area as a double

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,4 +1,7 @@
 // Mixin named Readable
+import 'dart:ffi';
+import 'dart:math';
+
 mixin Readable {
   // Method to print out a message indicating the book is being read
   void read() {
@@ -40,6 +43,18 @@ class User{
   // your code here
 }
 
+double calculateArea({required double length, required double width, String shape = "rectangle"}) {
+  double pi = 3.14;
+
+  if (shape == "circle") {
+    // Calculate the area of a circle using the length as the radius
+    return pi * pow(length, 2);
+  } else {
+    // Calculate the area of a rectangle (default case)
+    return length * width;
+  }
+}
+
 void main() {
   // Creating an instance of Book
   var book = Book('The Three-Body Problem', 'Liu Cixin', 400);
@@ -51,4 +66,15 @@ void main() {
 
   // Call mixin method
   fictionBook.read();
+
+
+  // Example usage of the calculateArea function
+
+  // Calculating the area of a rectangle
+  double rectangleArea = calculateArea(length: 5.0, width: 3.0);
+  print("Area of the rectangle: $rectangleArea");
+
+  // Calculating the area of a circle
+  double circleArea = calculateArea(length: 4.0, width: 0.0, shape: "circle");
+  print("Area of the circle: $circleArea");
 }
