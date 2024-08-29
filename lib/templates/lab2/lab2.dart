a5f6c0bdf68c0a9e655c869e6e1a4c3a6b7d9ca8
Tony <mr.tony.chulakov@gmail.com>
2024-08-29 19:35:59 +0300

    Lab 2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,15 +1,80 @@
-class Book {
-  // your code here
+void main() {
+  // Task 1
+  print('--------------------');
+  var book = Book('Moby Dick', 'Herman Melville', 200);
+  book.bookDetails();
+
+  // Task 2
+  print('--------------------');
+  var fictionBook = FictionBook('The Shadow over Innsmouth', 'H. P. Lovecraft', 300, 'Horror');
+  fictionBook.bookDetails();
+
+  // Task 3
+  print('--------------------');
+  book.read();
+
+  // Task 4
+  print('--------------------');
+  print('Area of a rectangle: ${calculateArea(length: 4, width: 5)}');
+  print('Area of a circle: ${calculateArea(length: 4, width: 4, shape: "circle")}');
+
+  // Task 5
+  print('--------------------');
+  User user1 = User(name: 'John Doe', age: 30, email: 'john@example.com');
+  user1.printUserDetails();
+
+  User user2 = User(name: 'Jane Doe', age: null, email: null);
+  user2.printUserDetails();
+}
+class Book with Readable {
+  final String title;
+  final String author;
+  final int numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('Title: $title, Author: $author, Number of Pages: $numberOfPages');
+  }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  final String genre;
+
+  FictionBook(String title, String author, int numberOfPages, this.genre)
+      : super(title, author, numberOfPages);
+
+  @override
+  void bookDetails() {
+    super.bookDetails();
+    print('Genre: $genre');
+  }
 }
 
-mixin JsonSerializable {
-  // your code here
+mixin Readable {
+  void read() {
+    print('The book is being read.');
+  }
 }
 
-class User{
-  // your code here
+double calculateArea({required double length, required double width, String shape = "rectangle"}) {
+  if (shape == "circle") {
+    return 3.14 * (length * length);
+  } else {
+    return length * width;
+  }
+}
+
+class User {
+  final String name;
+  final int? age;
+  final String? email;
+
+  User({required this.name, this.age, this.email});
+
+  void printUserDetails() {
+    print('Name: $name');
+    print('Age: ${age ?? 'N/A'}');
+    print('Email: ${email ?? 'N/A'}');
+  }
 }
