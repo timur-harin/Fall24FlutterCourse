282120a18acd44ca9af4be37b295bb1435f3fa8c
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-08-29 22:28:52 +0600

    lab2:inheritance
    Create a class named FictionBook that inherits from the Book class

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -11,8 +11,17 @@ class Book {
   }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  String genre;
+
+  // Constructor to initialize title, author, numberOfPages, and genre
+  FictionBook(super.title, super.author, super.numberOfPages, this.genre);
+  
+  // Override bookDetails method to include genre in the output
+  @override
+  void bookDetails() {
+    print('Title: $title, Author: $author, Number of Pages: $numberOfPages, Genre: $genre');
+  }
 }
 
 mixin JsonSerializable {
@@ -27,4 +36,8 @@ void main() {
   // Creating an instance of Book
   var book = Book('The Three-Body Problem', 'Liu Cixin', 400);
   book.bookDetails();
+
+  // Creating an instance of FictionBook
+  var fictionBook = FictionBook('Dune', 'Frank Herbert', 412, 'Science Fiction');
+  fictionBook.bookDetails();
 }
