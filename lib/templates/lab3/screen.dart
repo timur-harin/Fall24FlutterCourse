8c46ee1317a3e933e5df3ec6f1931ea883ee02bd
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-09-02 15:12:24 +0300

    lab3
    Create an enchanting Flutter-based app that guides users through the mystical world of hydration

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'hydration.dart';
+import 'notifier.dart';
 
 class HydrationScreen extends ConsumerWidget {
   const HydrationScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    // Add ref.watch and use provider to get the water intake
+    final waterIntake = ref.watch(waterIntakeProvider);
     return Scaffold(
-      // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      // AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: const Text('WaterBalance'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              // Reset the water intake using Riverpod notifier
+              ref.read(waterIntakeProvider.notifier).reset();
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TODO - Add text to display the water intake
-            // TODO add HydrationWidget to display the water intake and put waterIntake into it
-            // Add more UI components if necessary
+            // Display the current water intake level
+            Text(
+              'Water Intake: ${waterIntake.toStringAsFixed(2)} liters',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            // Display the HydrationWidget with the current water intake level
+            HydrationWidget(waterIntakeLevel: waterIntake),
           ],
         ),
       ),
-      // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Increment the water intake by 0.25 liters using Riverpod notifier
+          ref.read(waterIntakeProvider.notifier).increment(0.25);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
