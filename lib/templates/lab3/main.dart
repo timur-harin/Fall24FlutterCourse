971a725648e46414bfe3ec1f915a242018d8725d
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-09-02 16:43:51 +0300

    lab3

--- lib/templates/lab3/main.dart
+++ lib/templates/lab3/main.dart
@@ -1,10 +1,13 @@
+import 'package:fall_24_flutter_course/templates/lab3/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     // TODO to enable riverpod - add ProviderScope
-    WaterBalanceApp(),
+    const ProviderScope(
+      child: WaterBalanceApp(),
+    ),
   );
 }
 
@@ -19,7 +22,7 @@ class WaterBalanceApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // TODO to run app - change to needed screen widget
-      home: null,
+      home: const HydrationScreen(),
     );
   }
 }
--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -1,3 +1,5 @@
+import 'package:fall_24_flutter_course/templates/lab3/hydration.dart';
+import 'package:fall_24_flutter_course/templates/lab3/notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,20 +9,43 @@ class HydrationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
+
     return Scaffold(
       // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: const Text('Water level'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              ref.read(waterIntakeProvider.notifier).reset();
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TODO - Add text to display the water intake
-            // TODO add HydrationWidget to display the water intake and put waterIntake into it
-            // Add more UI components if necessary
+            Text(
+              'Water Intake: $waterIntake liters',
+              style: TextStyle(
+                  fontSize: 20, color: const Color.fromARGB(255, 3, 30, 77)),
+            ),
+            const SizedBox(height: 20),
+            HydrationWidget(waterIntakeLevel: waterIntake),
           ],
         ),
       ),
       // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(waterIntakeProvider.notifier).increment(0.2);
+        },
+        tooltip: 'Increase',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
