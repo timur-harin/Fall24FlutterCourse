a3a7bfff2cc56fc052e0de5f3ff2a26a3866c598
zDragonLORD <44378815+zDragonLORD@users.noreply.github.com>
2024-09-02 14:54:10 +0300

    Update screen.dart

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'hydration.dart';
+import 'notifier.dart';
 
 class HydrationScreen extends ConsumerWidget {
   const HydrationScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
+
     return Scaffold(
-      // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: const Text('Water Balance'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              ref.read(waterIntakeProvider.notifier).reset();
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TODO - Add text to display the water intake
-            // TODO add HydrationWidget to display the water intake and put waterIntake into it
-            // Add more UI components if necessary
+            Text(
+              'Water Intake: ${waterIntake.toStringAsFixed(2)} liters',
+              style: const TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            HydrationWidget(waterIntakeLevel: waterIntake),
           ],
         ),
       ),
-      // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(waterIntakeProvider.notifier).increment(0.25);
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
