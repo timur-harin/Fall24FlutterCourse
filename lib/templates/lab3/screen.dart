75b09bafa15e1464d0bf9d5b1e17071fa1383571
Said Nurullin <s.nurullin@innopolis.university>
2024-09-02 15:45:53 +0300

    lab 3 finished

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -1,3 +1,5 @@
+import 'package:fall_24_flutter_course/templates/lab3/hydration.dart';
+import 'package:fall_24_flutter_course/templates/lab3/notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,21 +8,40 @@ class HydrationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
     return Scaffold(
-      // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: const Text("Hydration tracker"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(waterIntakeProvider.notifier).reset();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TODO - Add text to display the water intake
-            // TODO add HydrationWidget to display the water intake and put waterIntake into it
-            // Add more UI components if necessary
+            Text(
+              'Water Intake: $waterIntake ml',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            HydrationWidget(waterIntakeLevel: waterIntake),
+            const SizedBox(height: 20),
           ],
         ),
       ),
-      // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(waterIntakeProvider.notifier).increment(250);
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
