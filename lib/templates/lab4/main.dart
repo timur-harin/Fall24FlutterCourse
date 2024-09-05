c95712c0669a4b26d296fbea1e3a5e22e1ac8c33
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-09-05 19:42:49 +0300

    lab4:complete

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' hide ChangeNotifierProvider, Provider;
+import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CounterProvider()), // Provider
+      ],
+      child: ProviderScope(child: MyApp()), // Riverpod
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        home: MyHomePage(),
-      ),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
+    final asyncResult = ref.watch(asyncResultProvider);
+    final httpResult = ref.watch(httpResultProvider);
+    final dioResult = ref.watch(dioResultProvider);
+    final counter = Provider.of<CounterProvider>(context).counter;
+    final counterNotifier = ref.watch(counterNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -29,40 +42,55 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Display the result of Async/Await Task
+            Text('Async Result: $asyncResult'),
             ElevatedButton(
               onPressed: () async {
-                // Exercise 1 - Perform an async operation using async/await
                 String result = await fetchData();
-                print(result);
+                ref.read(asyncResultProvider.notifier).state = result; // Update state
               },
               child: Text('Async/Await Task'),
             ),
+            // Display the result of Provider Task (simple counter)
+            Text('Provider Counter: $counter'),
             ElevatedButton(
               onPressed: () {
-                // Exercise 2 - Use Provider for state management
-                // Increment the counter
+                Provider.of<CounterProvider>(context, listen: false).increment();
               },
               child: Text('Provider Task'),
             ),
+            // Display the result of Riverpod Task (advanced counter)
+            Text('Riverpod Counter: $counterNotifier'),
             ElevatedButton(
               onPressed: () {
-                // TODO
-                // Exercise 3 - Use Riverpod for state management
-                // Increment the counter
+                ref.read(counterNotifierProvider.notifier).increment();
               },
               child: Text('Riverpod Task'),
             ),
+            // Display the result of HTTP Task
+            Text('HTTP Result: $httpResult'),
             ElevatedButton(
               onPressed: () async {
-                // TODO 
-                // Exercise 4 - Make an HTTP request using the HTTP package
+                final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+                if (response.statusCode == 200) {
+                  ref.read(httpResultProvider.notifier).state = response.body; // Update state
+                } else {
+                  ref.read(httpResultProvider.notifier).state = 'Failed to fetch data';
+                }
               },
               child: Text('HTTP Task'),
             ),
+            // Display the result of Dio Task
+            Text('Dio Result: $dioResult'),
             ElevatedButton(
               onPressed: () async {
-                // TODO
-                // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                try {
+                  Dio dio = Dio();
+                  final response = await dio.get('https://jsonplaceholder.typicode.com/posts/1');
+                  ref.read(dioResultProvider.notifier).state = response.data.toString(); // Update state
+                } catch (e) {
+                  ref.read(dioResultProvider.notifier).state = 'Dio Error: $e';
+                }
               },
               child: Text('Dio Task'),
             ),
@@ -73,10 +101,10 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// Task 1: Fetch data asynchronously using async/await
+// Fetch data asynchronously using async/await
 Future<String> fetchData() async {
   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
-  
+
   if (response.statusCode == 200) {
     return response.body; // returning the body of the response as a string
   } else {
@@ -84,9 +112,33 @@ Future<String> fetchData() async {
   }
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
+// Provider state management (simple counter)
+class CounterProvider with ChangeNotifier {
+  int _counter = 0;
 
-// TODO create a state notifier
-// final 
+  int get counter => _counter;
 
-// TODO create class for state notifier
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
+// Riverpod state management (advanced counter using StateNotifier)
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
+
+  void increment() {
+    state++;
+  }
+}
+
+final counterNotifierProvider = StateNotifierProvider<CounterNotifier, int>((ref) {
+  return CounterNotifier();
+});
+
+final asyncResultProvider = StateProvider<String>((ref) => 'No data yet');
+
+final httpResultProvider = StateProvider<String>((ref) => 'No data yet');
+
+final dioResultProvider = StateProvider<String>((ref) => 'No data yet');
