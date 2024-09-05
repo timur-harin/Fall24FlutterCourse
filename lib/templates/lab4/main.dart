a16a0e9108e35fe3d4505038cb6b40a670ff5a23
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-09-05 20:14:50 +0300

    lab4 completed

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
@@ -18,9 +19,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -31,39 +32,62 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             ElevatedButton(
               onPressed: () async {
-                // TODO
-                // Exercise 1 - Perform an async operation using async/await
-                String result = await fetchData();
-                print(result);
+                print('Fetching data...');
+                Map<String, dynamic> fetchedData = await fetchData();
+
+                if (fetchedData.containsKey('error')) {
+                  _showDialog(context, 'Error', fetchedData['error']);
+                } else {
+                  _showDialog(
+                      context, fetchedData['title'], fetchedData['body']);
+                }
               },
               child: Text('Async/Await Task'),
             ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                // Exercise 2 - Use Provider for state management
-                // Increment the counter
+                ref.read(counterState.notifier).state++;
+                _showDialog(context, 'Counter Value',
+                    ref.watch(counterState).toString());
               },
-              child: Text('Provider Task'),
+              child: Text('Provider Task = ${ref.watch(counterState)}'),
             ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                // TODO
-                // Exercise 3 - Use Riverpod for state management
-                // Increment the counter
+                ref.read(riverpodCounterState.notifier).incrementCounter();
+                _showDialog(context, 'Riverpod Counter Value',
+                    ref.watch(riverpodCounterState).toString());
               },
-              child: Text('Riverpod Task'),
+              child: Text('Riverpod Task = ${ref.watch(riverpodCounterState)}'),
             ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () async {
-                // TODO 
-                // Exercise 4 - Make an HTTP request using the HTTP package
+                print('Making HTTP request...');
+                Map<String, dynamic> httpResponse = await fetchDataAsMap();
+                if (httpResponse.containsKey('error')) {
+                  _showDialog(context, 'Error', httpResponse['error']);
+                } else {
+                  _showDialog(
+                      context, httpResponse['title'], httpResponse['body']);
+                }
               },
               child: Text('HTTP Task'),
             ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () async {
-                // TODO
-                // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                print('Making Dio request...');
+                Map<String, dynamic> dioResponse = await fetchDataDio();
+
+                if (dioResponse.containsKey('error')) {
+                  _showDialog(context, 'Error', dioResponse['error']);
+                } else {
+                  _showDialog(
+                      context, dioResponse['title'], dioResponse['body']);
+                }
               },
               child: Text('Dio Task'),
             ),
@@ -72,18 +96,91 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+
+  void _showDialog(BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(content),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<Map<String, dynamic>> fetchData() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+      if (response.statusCode == 200) {
+        // Decode the JSON data
+        return jsonDecode(response.body) as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      return {'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchDataAsMap() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+      if (response.statusCode == 200) {
+        // Decode the JSON data
+        return jsonDecode(response.body) as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      return {'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchDataDio() async {
+    try {
+      final dio = Dio();
+      final response =
+          await dio.get('https://jsonplaceholder.typicode.com/posts/1');
+
+      if (response.statusCode == 200) {
+        // Decode the JSON data
+        return response.data as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      return {'error': 'Error: $e'};
+    }
+  }
 }
 
-Future<String> fetchData() async {
-  // TODO get json from url and show as text
-  // 'https://jsonplaceholder.typicode.com/posts/1'
+final jsonState = StateProvider<String>((ref) => '');
+final counterState = StateProvider<int>((ref) => 0);
+final dioResponseState = StateProvider<String>((ref) => '');
+final httpResponseState = StateProvider<String>((ref) => '');
+final riverpodCounterState =
+    StateNotifierProvider<CounterNotifier, int>((ref) => CounterNotifier());
 
-  return 'data';
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
+  void incrementCounter() {
+    state++;
+  }
 }
-
-final counterProvider = StateProvider<int>((ref) => 0);
-
-// TODO create a state notifier
-// final 
-
-// TODO create class for state notifier
