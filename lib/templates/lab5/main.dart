a8100fe96612980616ae6a309bf6f7520431f8d4
sonyivanova <s.ivanova@innopolis.university>
2024-09-09 16:44:27 +0300

    lab5

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,18 +1,140 @@
 // Use these dependencies for your classes
 import 'dart:convert';
+import 'package:fall_24_flutter_course/templates/lab5/comment.dart';
+import 'package:fall_24_flutter_course/templates/lab5/post.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 // TODO add needed classes for Flutter APP
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: _generateRoute,
+      initialRoute: '/',
+    );
+  }
 
-// TODO add generated route flutter app with undifined page with cat status code using api
+  // TODO add generated route flutter app with undifined page with cat status code using api
+  Route<dynamic>? _generateRoute(RouteSettings settings){
+    switch (settings.name){
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/postDetails':
+        final postId = settings.arguments as int;
+        return MaterialPageRoute(builder: (_) => PostDetailsPage(postId: postId));
+      default:
+        final uri = Uri.parse('https://http.cat/${settings.name?.substring(1)}');
+        return MaterialPageRoute(
+          builder: (_) => UndefinedPage(uri: uri),
+        );
+    }
+  }
+}
 
-// TODO add putting argument in route navigation as parameter for generated page
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/postDetails',
+              arguments: 1,
+            );
+          },
+          child: Text('Go to Post Details'),
+        ),
+      ),
+    );
+  }
+}
 
-// TODO use api with cat status codes
-// https://http.cat/[status_code]
+class PostDetailsPage extends StatelessWidget {
+  final int postId;
+
+  PostDetailsPage({required this.postId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Post Details')),
+      body: FutureBuilder(
+        future: Future.wait([fetchPosts(), fetchComments()]), 
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No post found.'));
+          } else {
+            final posts = snapshot.data![0] as List<Post>;
+            final comments = snapshot.data![1] as List<Comment>;
+
+            final post = posts.firstWhere((post) => post.id == postId,
+              orElse: () => Post(userId: 0, id: 0, title: 'Not Found', body: 'No content')
+            );
+
+            final postComment = comments.where((comment) => comment.postId == post.id).toList();
+
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(post.title, style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )),
+                    const SizedBox(height: 10),
+                    Text(post.body),
+                    const SizedBox(height: 15),
+                    const Text ('Comments:', style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )),
+                    ...postComment.map((comment){
+                      return ListTile(
+                        title: Text(comment.name),
+                        subtitle: Text(comment.body),
+                      );
+                    }),
+                  ],
+                )
+              ),
+            );
+          }
+        }
+      ),
+    );
+  }
+}
+
+class UndefinedPage extends StatelessWidget {
+  final Uri uri;
+
+  UndefinedPage({required this.uri});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Undefined Page')),
+      body: Center(
+        child: Image.network(uri.toString()),
+      ),
+    );
+  }
+}
 
--- lib/templates/lab5/user.dart
+++ lib/templates/lab5/user.dart
@@ -3,15 +3,35 @@
 // TODO add user.freezed.dart as part
 
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// @freezed
-// class User with _$User {
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
   // TODO task 3 to make this class for url http://jsonplaceholder.typicode.com/users/
-// }
 
-// Future<List<User>> fetchUsers() async {
+  const factory User({
+    required int id,
+    required String name,
+    required String email,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
+
+Future<List<User>> fetchUsers() async {
   // TODO task 3.2 to make this function for url http://jsonplaceholder.typicode.com/users/ 
   // Using fabric from class
 
-  // return [];
-// }
+  final response = await http.get(Uri.parse('http://jsonplaceholder.typicode.com/users'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> userJson = jsonDecode(response.body);
+    return userJson.map((json) => User.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load users');
+  }
+}
