14010b8dd91112ed11fc3fb2352c7cd50b5e4e5f
Louay Farah <l.farah@innopolis.university>
2024-09-09 14:56:57 +0100

    Refactor User class to use freezed_annotation in user.dart

--- lib/templates/lab5/user.dart
+++ lib/templates/lab5/user.dart
@@ -1,37 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class User {
-  final int id;
-  final String name;
-  final String username;
-  final String email;
-  final Map<String, dynamic> address;
-  final String phone;
-  final String website;
-  final Map<String, dynamic> company;
+part 'user.freezed.dart';
+part 'user.g.dart';
 
-  User(
-      {required this.id,
-      required this.name,
-      required this.username,
-      required this.email,
-      required this.address,
-      required this.phone,
-      required this.website,
-      required this.company});
+@freezed
+class User with _$User {
+  factory User({
+    required int id,
+    required String name,
+    required String username,
+    required String email,
+    required Map<String, dynamic> address,
+    required String phone,
+    required String website,
+    required Map<String, dynamic> company,
+  }) = _User;
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-        id: json['id'],
-        name: json['name'],
-        username: json['username'],
-        email: json['email'],
-        address: json['address'],
-        phone: json['phone'],
-        website: json['website'],
-        company: json['company']);
-  }
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
 
 Future<List<User>> fetchUsers() async {
