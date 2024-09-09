85c20daacf3d7647a7c0cdbea0a0f7ac156c433f
anyar <anyarylova51@gmail.com>
2024-09-09 16:58:21 +0300

    lab5

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,18 +1,90 @@
-// Use these dependencies for your classes
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'comment.dart';
+import 'post.dart';
+import 'user.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: _onGenerateRoute,
+      home: HomePage(),
+    );
+  }
 
-// TODO add needed classes for Flutter APP
+  Route<dynamic> _onGenerateRoute(RouteSettings settings) {
+    final Uri uri = Uri.parse(settings.name ?? '');
+    if (uri.pathSegments.first == 'cat' && uri.pathSegments.length == 2) {
+      final status_code = uri.pathSegments[1];
+      return MaterialPageRoute(builder: (context) => StatusPage(status_code: status_code));
+    }
+    return MaterialPageRoute(builder: (context) => UndefinedPage());
+  }
+}
+class UndefinedPage extends StatelessWidget {
+  final catUrl = 'https://http.cat/404';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page is not defined', style: TextStyle(color: Colors.white, fontSize: 30),),
+        foregroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Image.network(catUrl),
+      ),
+    );
+  }
+}
 
-// TODO add generated route flutter app with undifined page with cat status code using api
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page', style: TextStyle(color: Colors.white, fontSize: 30),),
+        foregroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/', arguments: 404);
+              },
+              child: Text('Status Code 404'),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
 
+class StatusPage extends StatelessWidget {
+  String status_code;
+  StatusPage({required this.status_code});
+  @override
+  Widget build(BuildContext context) {
+    final catUrl = 'https://http.cat/$status_code';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cat Status Code: $status_code', style: TextStyle(color: Colors.white, fontSize: 30),),
+        foregroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Image.network(catUrl),
+      ),
+    );
+  }
+}
 // TODO add putting argument in route navigation as parameter for generated page
-
-// TODO use api with cat status codes
-// https://http.cat/[status_code]
-
--- lib/templates/lab5/post.dart
+++ lib/templates/lab5/post.dart
@@ -1,15 +1,52 @@
-// TODO add dependencies
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
   // TODO task 1 to make this class for url http://jsonplaceholder.typicode.com/posts
 
   // factory Post.fromJson(Map<String, dynamic> json) {}
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
 
+  Map<String, dynamic> toJson(Post post) {
+    return {
+      'userId': jsonEncode(post.userId),
+      'id': post.id,
+      'title': post.title,
+      'body': post.body,
+    };
+    
+  }
   // factory Post.toJson(Post post) {}
 }
 
 Future<List<Post>> fetchPosts() async {
   // TODO task 1.2 to make this function for url http://jsonplaceholder.typicode.com/posts
   // // Using fabric from class
-  return [];
+
+  final response = await http.get(Uri.parse('http://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> jsonData = jsonDecode(response.body);
+    return jsonData.map((json) => Post.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
 }
