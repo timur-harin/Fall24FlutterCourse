28f9feb54377e78542399efa3c03c9b1d820879f
Adel Shagaliev <a.shagaliev@innopolis.university>
2024-09-09 16:46:23 +0300

    feat: completed lab5

--- lib/templates/lab5/home_page.dart
+++ lib/templates/lab5/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dio/dio.dart';
 import 'package:fall_24_flutter_course/templates/lab5/app_router/app_router.gr.dart';
 import 'package:flutter/material.dart';
 
@@ -28,26 +29,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter status_code number: ',
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter status_code number: ',
+              ),
             ),
-          ),
-          
-          MaterialButton(
-            onPressed: () => context.pushRoute(
-              CatRoute(statusCode: _controller.text),
+            MaterialButton(
+              onPressed: () => context.pushRoute(
+                CatRoute(statusCode: _controller.text),
+              ),
+              child: const Text('Navigate to cat status_code page!'),
             ),
-            child: const Text('Navigate to cat status_code page!'),
-          ),
-        ],
+            MaterialButton(
+              onPressed: () => _execute(context, 'https://jsonplaceholder.typicode.com/comments'),
+              child: const Text('fetch comments!'),
+            ),
+            MaterialButton(
+              onPressed: () => _execute(context, 'https://jsonplaceholder.typicode.com/users'),
+              child: const Text('fetch users!'),
+            ),
+            MaterialButton(
+              onPressed: () => _execute(context, 'https://jsonplaceholder.typicode.com/posts'),
+              child: const Text('fetch posts!'),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  Future<void> _execute(BuildContext context, String path) async {
+    final Dio dio = Dio();
+    final Response response =
+        await dio.get('https://jsonplaceholder.typicode.com/comments');
+    if (context.mounted) {
+      context.pushRoute(CatRoute(statusCode: response.statusCode.toString()));
+    }
   }
 }
