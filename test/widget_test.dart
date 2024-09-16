42a23a607882f3e915478fa2560536a00386aa6f
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-09-16 15:55:16 +0300

    lab7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,11 @@
 import 'package:fall_24_flutter_course/templates/lab7/widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('MyWidget has a title', (WidgetTester tester) async {
-    // TODO add pumpWidget with title
-   
-    // TODO add titleFinder with find.text
-
-    // TODO add expect with findsOneWidget
+    await tester.pumpWidget(MyWidget(title: 'Test Title'));
+    final titleFinder = find.text('Test Title');
+    expect(titleFinder, findsOneWidget);
   });
 }
