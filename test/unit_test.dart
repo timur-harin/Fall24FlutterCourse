91c2c32078d749278d78d1bda1897ea29da68e42
zDragonLORD <44378815+zDragonLORD@users.noreply.github.com>
2024-09-16 15:32:35 +0300

    Update unit_test.dart

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,36 +1,30 @@
-
-
 import 'package:fall_24_flutter_course/templates/lab7/calculator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  final calculator = Calculator();
+
   test('adds two numbers', () {
-    final calculator = Calculator();
-    expect(calculator.add(1, 2), 4);
+    expect(calculator.add(1, 2), 3);
   });
 
   test('subtract two numbers', () {
-    final calculator = Calculator();
-    // TODO add expect
+    expect(calculator.subtract(5, 3), 2);
   });
 
   test('multiply two numbers', () {
-    final calculator = Calculator();
-    // TODO add expect
+    expect(calculator.multiply(4, 3), 12);
   });
 
   test('divide two numbers', () {
-    final calculator = Calculator();
-    // TODO add expect
+    expect(calculator.divide(8, 4), 2);
   });
 
   test('mod two numbers', () {
-    final calculator = Calculator();
-    // TODO add expect
+    expect(calculator.mod(10, 3), 1);
   });
 
   test('power two numbers', () {
-    final calculator = Calculator();
-    // TODO add expect
+    expect(calculator.power(2, 3), 8);
   });
 }
