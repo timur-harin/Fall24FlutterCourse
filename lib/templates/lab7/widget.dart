71c0dcbcdad6c1c361e3e60a9727fd178ff48fdb
MattWay224 <mattew04@mail.ru>
2024-09-16 14:22:38 +0300

    Added more widgets for testing

--- lib/templates/lab7/widget.dart
+++ lib/templates/lab7/widget.dart
@@ -12,8 +12,28 @@ class MyWidget extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text('Hello World'),
+            ),
+            const SizedBox(height: 20),
+            Slider(
+              value: 0.5,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 20),
+            Checkbox(
+              value: false,
+              onChanged: (bool? value) {},
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Press Me'),
+            ),
+          ],
         ),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,24 @@
 import 'package:fall_24_flutter_course/templates/lab7/widget.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('MyWidget has a title', (WidgetTester tester) async {
+  testWidgets('MyWidget has a title and random widgets', (WidgetTester tester) async {
     await tester.pumpWidget(MyWidget(title: 'Test Title'));
 
     final titleFinder = find.text('Test Title');
-
     expect(titleFinder, findsOneWidget);
+
+    final helloWorldFinder = find.text('Hello World');
+    expect(helloWorldFinder, findsOneWidget);
+
+    final sliderFinder = find.byType(Slider);
+    expect(sliderFinder, findsOneWidget);
+
+    final checkboxFinder = find.byType(Checkbox);
+    expect(checkboxFinder, findsOneWidget);
+
+    final buttonFinder = find.widgetWithText(ElevatedButton, 'Press Me');
+    expect(buttonFinder, findsOneWidget);
   });
 }
