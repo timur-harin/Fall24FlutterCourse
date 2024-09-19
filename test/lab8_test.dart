0330f7373ff87264803bee2a0f0689b4899ba978
zDragonLORD <44378815+zDragonLORD@users.noreply.github.com>
2024-09-19 13:30:08 +0300

    Create lab8_test.dart

---
+++ test/lab8_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fall_24_flutter_course/templates/lab8/main.dart';
+
+void main() {
+  testWidgets('Displays "Hello World!" text', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MaterialApp(home: MyHomePage())));
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
