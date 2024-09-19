7f6f3bb814945ff6907886eb0a19954bf7c88398
Damncaf-goin-crazy <Decafattic@gmail.com>
2024-09-19 19:42:12 +0300

    lab 8

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,9 +4,12 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('MyWidget has a title', (WidgetTester tester) async {
     // TODO add pumpWidget with title
-   
+    await tester.pumpWidget(MyWidget(title: 'Test Title'));
+
     // TODO add titleFinder with find.text
+    final titleFinder = find.text('Test Title');
 
     // TODO add expect with findsOneWidget
+    expect(titleFinder, findsOneWidget);
   });
 }
