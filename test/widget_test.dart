9dfb96e2cbc2defbfb603055e8921a16333af4e6
Evgeny Bobkunov <jony.zh@yandex.ru>
2024-09-19 17:52:21 +0300

    lab8:fix test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:your_project/main.dart';
+import 'package:fall_24_flutter_course/templates/lab8/main.dart';
 
 void main() {
   testWidgets('Test if Hello World is displayed', (WidgetTester tester) async {
