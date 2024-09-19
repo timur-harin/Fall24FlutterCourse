8a9820b9273ff16308773039e8f66ff4e9cb8f2c
Galia <g1l4a>
2024-09-19 20:06:03 +0300

    lab 8

---
+++ test/widget_test_lab8.dart
@@ -0,0 +1,11 @@
+import 'package:fall_24_flutter_course/templates/lab8/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  testWidgets('Main page displays Hello World text', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MaterialApp()));
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
