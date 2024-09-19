bb6f106a6cea53c661dc57bb538f3af1369d02f2
maksalena <maksalena04@mail.ru>
2024-09-19 20:21:25 +0300

    lab8

---
+++ test/main_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fall_24_flutter_course/templates/lab8/main.dart'; 
+
+void main() {
+  testWidgets('MyHomePage has Hello World text', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
