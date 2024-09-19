d10bd98dfe45a6d644e38bbb13c30afd97f39c61
Durmazz1 <almaz.gayazov.04@yandex.ru>
2024-09-19 18:12:31 +0300

    lab-7

---
+++ lib/templates/lab8/widget_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fall_24_flutter_course/templates/lab8/main.dart';
+
+void main() {
+  testWidgets('Main page displays "Hello flutter"', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
+    expect(find.text('Hello flutter'), findsOneWidget);
+  });
+}
