94114a38a78bfb383d4746e9a1fc25164f82fd29
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2024-09-19 20:27:02 +0300

    Update wid_test.dart

--- test/wid_test.dart
+++ test/wid_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'main.dart';
+import 'lib/templates/lab8/main.dart';
 
 void main() {
   testWidgets('MyHomePage has a Hello World text', (WidgetTester tester) async {
