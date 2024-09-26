ced8ce5d070f8965c50bc1c7e3a0333434289c41
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2024-09-26 18:52:34 +0300

    lab5

--- lib/templates/lab5/user.dart
+++ lib/templates/lab5/user.dart
@@ -1,17 +1,71 @@
-// TODO add dependencies
-// TODO add user.g.dart as part
-// TODO add user.freezed.dart as part
-
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// @freezed
-// class User with _$User {
-  // TODO task 3 to make this class for url http://jsonplaceholder.typicode.com/users/
-// }
+part 'user.g.dart';
+part 'user.freezed.dart';
 
-// Future<List<User>> fetchUsers() async {
-  // TODO task 3.2 to make this function for url http://jsonplaceholder.typicode.com/users/ 
-  // Using fabric from class
 
-  // return [];
-// }
+@freezed
+class User with _$User {
+  factory User({
+    required int id,
+    required String name,
+    required String username,
+    required String email,
+    required Address address,
+    required String phone,
+    required String website,
+    required Company company,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
+
+@freezed
+class Address with _$Address {
+  factory Address({
+    required String street,
+    required String suite,
+    required String city,
+    required String zipcode,
+    required Geo geo,
+  }) = _Address;
+
+  factory Address.fromJson(Map<String, dynamic> json) => _$AddressFromJson(json);
+}
+
+@freezed
+class Geo with _$Geo {
+  factory Geo({
+    required String lat,
+    required String lng,
+  }) = _Geo;
+
+  factory Geo.fromJson(Map<String, dynamic> json) => _$GeoFromJson(json);
+}
+
+@freezed
+class Company with _$Company {
+  factory Company({
+    required String name,
+    required String catchPhrase,
+    required String bs,
+  }) = _Company;
+
+  factory Company.fromJson(Map<String, dynamic> json) => _$CompanyFromJson(json);
+}
+
+
+Future<List<User>> fetchUsers() async {
+  const url = 'http://jsonplaceholder.typicode.com/users/';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> usersJson = jsonDecode(response.body);
+    return usersJson.map((json) => User.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load users');
+  }
+}
