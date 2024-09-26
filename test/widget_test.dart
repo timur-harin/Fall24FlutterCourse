0f1fb01ba41f8a4e92c2b93eaac8b8347564a1a5
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2024-09-26 19:39:23 +0300

    lab8

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,13 @@
-import 'package:fall_24_flutter_course/templates/lab7/widget.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:fall_24_flutter_course/templates/lab8/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const _title = "test";
 
 void main() {
-  testWidgets('MyWidget has a title', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: MyWidget(title: 'Test Title'),
-    ));
-    final titleFinder = find.text('Test Title');
+  testWidgets('MyHomePage has a title', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHomePage(title: _title)));
+    final titleFinder = find.text(_title);
     expect(titleFinder, findsOneWidget);
   });
 }
